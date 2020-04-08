76ba05e1d25d2e3dc24aa562f875e201a5aa7504
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2020-04-08 18:16:49 -0400

    use machine in test

--- src/components/AlertBar.spec.js
+++ src/components/AlertBar.spec.js
@@ -5,24 +5,35 @@ import AlertBar from './AlertBar'
 import { snackbarMachine } from "../machines/snackbarMachine";
 
 describe('AlertBar', () => {
-  it('loads', () => {
+  it.skip('loads with dummy mock', () => {
     cy.viewport(400, 100)
-    // const snackbarState = {
-    //   changed: true,
-    //   matches (state) {
-    //     return state === 'visible'
-    //   },
-    //   context: {
-    //     severity: 'info',
-    //     message: 'Hi pay-app from from cypress-react-unit-test!'
-    //   }
-    // }
-    // const snackbarService = {
-    //   subscribe (cb) {
-    //     return cb(snackbarState)
-    //   }
-    // }
-    // const [, , snackbarService] = useMachine(snackbarMachine)
-    // mount(<AlertBar snackbarService={snackbarService} />)
+    const snackbarState = {
+      changed: true,
+      matches (state) {
+        return state === 'visible'
+      },
+      context: {
+        severity: 'info',
+        message: 'Hi pay-app from from cypress-react-unit-test!'
+      }
+    }
+    const snackbarService = {
+      subscribe (cb) {
+        return cb(snackbarState)
+      }
+    }
+    mount(<AlertBar snackbarService={snackbarService} />)
+  })
+
+  it('loads with xstate machine', () => {
+    cy.viewport(400, 100)
+
+    const TestAlertBar = () => {
+      const [, send, snackbarService] = useMachine(snackbarMachine)
+      send('SHOW', {severity: 'info', message: 'Hello from XState!'})
+      return <AlertBar snackbarService={snackbarService} />
+    }
+    mount(<TestAlertBar />)
+    cy.contains('Hello from XState!').should('be.visible')
   })
 })
