78eb1ab54b27678135398d035c92c913c9dd4f48
Kevin Old <kevin@cypress.io>
2020-06-29 10:46:37 -0500

    add route logging

--- backend/app.ts
+++ backend/app.ts
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { Application } from "express";
 import path from "path";
 import logger from "morgan";
 import passport from "passport";
@@ -70,5 +70,51 @@ if (process.env.NODE_ENV === "test" || process.env.NODE_ENV === "development") {
 console.log("before static route");
 
 app.use(express.static(path.join(__dirname, "../public")));
+
+function split(thing: any): string {
+  if (typeof thing === "string") {
+    return thing;
+  } else if (thing.fast_slash) {
+    return "";
+  } else {
+    var match = thing
+      .toString()
+      .replace("\\/?", "")
+      .replace("(?=\\/|$)", "$")
+      .match(/^\/\^((?:\\[.*+?^${}()|[\]\\\/]|[^.*+?^${}()|[\]\\\/])*)\$\//);
+    return match ? match[1].replace(/\\(.)/g, "$1") : "<complex:" + thing.toString() + ">";
+  }
+}
+
+function getRoutesOfLayer(path: string, layer: any): string[] {
+  if (layer.method) {
+    return [layer.method.toUpperCase() + " " + path];
+  } else if (layer.route) {
+    return getRoutesOfLayer(path + split(layer.route.path), layer.route.stack[0]);
+  } else if (layer.name === "router" && layer.handle.stack) {
+    let routes: string[] = [];
+
+    layer.handle.stack.forEach(function (stackItem: any) {
+      routes = routes.concat(getRoutesOfLayer(path + split(layer.regexp), stackItem));
+    });
+
+    return routes;
+  }
+
+  return [];
+}
+
+function getRoutes(app: Application): string[] {
+  let routes: string[] = [];
+
+  app._router.stack.forEach(function (layer: any) {
+    routes = routes.concat(getRoutesOfLayer("", layer));
+  });
+
+  return routes;
+}
+
+const routes = getRoutes(app);
+console.log("ROUTES", routes);
 
 app.listen(3001);
