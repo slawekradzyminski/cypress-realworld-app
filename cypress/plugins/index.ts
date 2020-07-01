e2530eb9f7c530c65aa4aefd8826957be6b63a1d
Kevin Old <kevin@cypress.io>
2020-07-01 17:03:46 -0500

    cleanup

--- cypress/plugins/index.ts
+++ cypress/plugins/index.ts
@@ -2,18 +2,10 @@ import _ from "lodash";
 import axios from "axios";
 import dotenv from "dotenv";
 import Promise from "bluebird";
-//import { WebAuth } from "auth0-js";
 import codeCoverageTask from "@cypress/code-coverage/task";
 
 dotenv.config();
 dotenv.config({ path: ".env.dev" });
-
-/*
-const auth = new WebAuth({
-  domain: process.env.AUTH0_DOMAIN!,
-  clientID: process.env.AUTH0_CLIENTID!,
-});
-*/
 
 export default (on, config) => {
   config.env.defaultPassword = process.env.SEED_DEFAULT_USER_PASSWORD;
@@ -51,37 +43,6 @@ export default (on, config) => {
     "find:database"(queryPayload) {
       return queryDatabase(queryPayload, (data, attrs) => _.find(data.results, attrs));
     },
-    /*
-    "auth0:getUserInfo"(accessToken) {
-      auth.client.userInfo(accessToken, (err, user) => {
-        if (err) {
-          console.log(err);
-        }
-
-        return user;
-      });
-    },
-    "auth0:login"(username, password) {
-      auth.client.login(
-        {
-          realm: "Username-Password-Authentication",
-          username,
-          password,
-          scope: process.env.AUTH0_SCOPE,
-          audience: process.env.AUTH0_AUDIENCE,
-          // @ts-ignore
-          client_secret: process.env.AUTH0_CLIENTSECRET,
-        },
-        (err, response) => {
-          if (err) {
-            console.log(err);
-          } else {
-            return response;
-          }
-        }
-      );
-    },
-    */
   });
 
   codeCoverageTask(on, config);
