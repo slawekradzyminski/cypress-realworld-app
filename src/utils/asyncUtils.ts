59391c450923605189f533083f4a3dbfda9637df
Kevin Old <kevin@cypress.io>
2020-07-10 12:09:02 -0500

    cleanup

--- src/utils/asyncUtils.ts
+++ src/utils/asyncUtils.ts
@@ -1,12 +1,16 @@
+import dotenv from "dotenv";
 import axios from "axios";
+
+dotenv.config({ path: ".env.local" });
+dotenv.config();
 
 const httpClient = axios.create({
   withCredentials: true,
 });
 
 httpClient.interceptors.request.use((config) => {
-  const accessToken = localStorage.getItem("accessToken");
-  if (accessToken) {
+  if (process.env.REACT_APP_AUTH0) {
+    const accessToken = localStorage.getItem(process.env.REACT_APP_AUTH0_TOKEN_NAME);
     config.headers["Authorization"] = `Bearer ${accessToken}`;
   }
   return config;
