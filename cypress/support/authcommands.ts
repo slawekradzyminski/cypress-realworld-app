de1b45c57d6bc15733fcc0b16ac1c79f7195665b
Agata Adamczewska <aadamczewska@prograils.com>
2022-04-24 12:00:44 +0200

    fix

---
+++ cypress/support/authcommands.ts
@@ -0,0 +1,129 @@
+// @ts-check
+///<reference path="../global.d.ts" />
+
+
+import { isMobile } from "./utils";
+
+// Import Cypress Percy plugin command (https://docs.percy.io/docs/cypress)
+import "@percy/cypress";
+
+
+Cypress.Commands.add("login", (username, password, { rememberUser = false } = {}) => {
+  const signinPath = "/signin";
+  const log = Cypress.log({
+    name: "login",
+    displayName: "LOGIN",
+    message: [`🔐 Authenticating | ${username}`],
+    // @ts-ignore
+    autoEnd: false,
+  });
+
+  cy.intercept("POST", "/login").as("loginUser");
+  cy.intercept("GET", "checkAuth").as("getUserProfile");
+
+  cy.location("pathname", { log: false }).then((currentPath) => {
+    if (currentPath !== signinPath) {
+      cy.visit(signinPath);
+    }
+  });
+
+  log.snapshot("before");
+
+  cy.getBySel("signin-username").type(username);
+  cy.getBySel("signin-password").type(password);
+
+  if (rememberUser) {
+    cy.getBySel("signin-remember-me").find("input").check();
+  }
+
+  cy.getBySel("signin-submit").click();
+  cy.wait("@loginUser").then((loginUser: any) => {
+    log.set({
+      consoleProps() {
+        return {
+          username,
+          password,
+          rememberUser,
+          userId: loginUser.response.statusCode !== 401 && loginUser.response.body.user.id,
+        };
+      },
+    });
+
+    log.snapshot("after");
+    log.end();
+  });
+});
+
+Cypress.Commands.add("loginByApi", (username, password = Cypress.env("defaultPassword")) => {
+  return cy.request("POST", `${Cypress.env("apiUrl")}/login`, {
+    username,
+    password,
+  });
+});
+
+
+
+
+Cypress.Commands.add("loginByXstate", (username, password = Cypress.env("defaultPassword")) => {
+  const log = Cypress.log({
+    name: "loginbyxstate",
+    displayName: "LOGIN BY XSTATE",
+    message: [`🔐 Authenticating | ${username}`],
+    autoEnd: false,
+  });
+
+  cy.intercept("POST", "/login").as("loginUser");
+  cy.intercept("GET", "/checkAuth").as("getUserProfile");
+  cy.visit("/signin", { log: false }).then(() => {
+    log.snapshot("before");
+  });
+
+  cy.window({ log: false }).then((win) => win.authService.send("LOGIN", { username, password }));
+
+  cy.wait("@loginUser").then((loginUser) => {
+    log.set({
+      consoleProps() {
+        return {
+          username,
+          password,
+          // @ts-ignore
+          userId: loginUser.response.body.user.id,
+        };
+      },
+    });
+  });
+
+  return cy
+    .getBySel("list-skeleton")
+    .should("not.exist")
+    .then(() => {
+      log.snapshot("after");
+      log.end();
+    });
+});
+
+Cypress.Commands.add("logoutByXstate", () => {
+  const log = Cypress.log({
+    name: "logoutByXstate",
+    displayName: "LOGOUT BY XSTATE",
+    message: [`🔒 Logging out current user`],
+    // @ts-ignore
+    autoEnd: false,
+  });
+
+  cy.window({ log: false }).then((win) => {
+    log.snapshot("before");
+    win.authService.send("LOGOUT");
+  });
+
+  return cy
+    .location("pathname")
+    .should("equal", "/signin")
+    .then(() => {
+      log.snapshot("after");
+      log.end();
+    });
+});
+
+
+
---
+++ cypress/support/reactcommands.ts
@@ -0,0 +1,23 @@
+Cypress.Commands.add("reactComponent", { prevSubject: "element" }, ($el) => {
+    if ($el.length !== 1) {
+      throw new Error(`cy.component() requires element of length 1 but got ${$el.length}`);
+    }
+    // Query for key starting with __reactInternalInstance$ for React v16.x
+    //
+    const key = Object.keys($el.get(0)).find((key) => key.startsWith("__reactFiber$"));
+  
+    // @ts-ignore
+    const domFiber = $el.prop(key);
+  
+    Cypress.log({
+      name: "component",
+      consoleProps() {
+        return {
+          component: domFiber,
+        };
+      },
+    });
+  
+    return domFiber.return;
+  });
+  
