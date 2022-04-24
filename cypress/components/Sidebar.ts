ab574e44eae1a8bb0d09752cea446a5bff9396fb
Agata Adamczewska <aadamczewska@prograils.com>
2022-04-24 14:11:33 +0200

    xx

---
+++ cypress/components/Sidebar.ts
@@ -0,0 +1,33 @@
+
+
+import { isMobile } from "../support/utils";
+ 
+export enum Settings {
+ myAccount = "My Account",
+ home = "Home",
+ backAccounts = "Bank Accounts",
+ notifications = "Notifications",
+ logout = "Logout",
+}
+ 
+export default class Sidebar {
+ click(_value: Settings) {
+   if (isMobile()) {
+     cy.getBySel("sidenav-toggle").click();
+   }
+   switch (_value) {
+     case Settings.home:
+       return cy.getBySel("sidenav-home").click();
+     case Settings.myAccount:
+       return cy.getBySel("sidenav-user-settings").click();
+     case Settings.backAccounts:
+       return cy.getBySel("sidenav-bankaccounts").click();
+     case Settings.notifications:
+       return cy.getBySel("sidenav-notifications").click();
+     case Settings.logout:
+       return cy.getBySel("sidenav-signout").click();
+   }
+ }
+}
+ 
+
