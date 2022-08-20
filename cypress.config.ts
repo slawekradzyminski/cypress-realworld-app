f3c3226bd0e11b7796d1b61e4c6d145c5311a1ae
slawekradzyminski <slawomir.radzyminski@gmail.com>
2022-08-20 14:15:31 +0200

    bumped Cypress to 10.6.0

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,7 +1,7 @@
-import { defineConfig } from 'cypress'
+import { defineConfig } from "cypress";
 
 export default defineConfig({
-  projectId: '7s5okt',
+  projectId: "7s5okt",
   viewportHeight: 1000,
   viewportWidth: 1280,
   retries: {
@@ -9,21 +9,20 @@ export default defineConfig({
     openMode: 1,
   },
   env: {
-    apiUrl: 'http://localhost:3001',
+    apiUrl: "http://localhost:3001",
     mobileViewportWidthBreakpoint: 414,
     coverage: false,
     codeCoverage: {
-      url: 'http://localhost:3001/__coverage__',
+      url: "http://localhost:3001/__coverage__",
     },
   },
-  experimentalStudio: true,
   e2e: {
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
     setupNodeEvents(on, config) {
-      return require('./cypress/plugins/index.ts').default(on, config)
+      return require("./cypress/plugins/index.ts").default(on, config);
     },
-    baseUrl: 'http://localhost:3000',
-    specPattern: 'cypress/tests/**/*.{js,jsx,ts,tsx}',
+    baseUrl: "http://localhost:3000",
+    specPattern: "cypress/tests/**/*.{js,jsx,ts,tsx}",
   },
-})
+});
