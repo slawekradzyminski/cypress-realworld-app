78163913094837fd0a0b92bac68665d53a008086
slawekradzyminski <slawomir.radzyminski@gmail.com>
2022-08-21 09:06:24 +0200

    small refactor

---
+++ cypress/pages/BankAccounts.ts
@@ -0,0 +1,84 @@
+/* eslint-disable prettier/prettier */
+export default class BankAccountsPage {
+  verifyTooLongAccountNumberAlert() {
+    cy.getBySelLike("accountNumber-input").type("123456789111").find("input").blur();
+    cy.get("#bankaccount-accountNumber-input-helper-text").should("not.exist");
+    cy.getBySelLike("accountNumber-input").find("input").clear();
+
+    cy.getBySelLike("accountNumber-input").type("1234567891111").find("input").blur();
+    cy.get("#bankaccount-accountNumber-input-helper-text")
+      .should("be.visible")
+      .and("contain", "Must contain no more than 12 digits");
+  }
+
+  verifyTooShortAccountNumberAlert() {
+    cy.getBySelLike("accountNumber-input").type("12345678").find("input").blur();
+    cy.get("#bankaccount-accountNumber-input-helper-text")
+      .should("be.visible")
+      .and("contain", "Must contain at least 9 digits");
+    cy.getBySelLike("accountNumber-input").find("input").clear();
+
+    cy.getBySelLike("accountNumber-input").type("123456789").find("input").blur();
+    cy.get("#bankaccount-accountNumber-input-helper-text").should("not.exist");
+    cy.getBySelLike("accountNumber-input").find("input").clear();
+  }
+
+  verifyEmptyAccountNumberAlert() {
+    cy.getBySelLike("accountNumber-input").find("input").focus().blur();
+    cy.get(`#bankaccount-accountNumber-input-helper-text`)
+      .should("be.visible")
+      .and("contain", "Enter a valid bank account number");
+  }
+
+  verifyTooShortRoutingNumberAlert() {
+    cy.getBySelLike("routingNumber-input").type("12345678").find("input").blur();
+    cy.get("#bankaccount-routingNumber-input-helper-text")
+      .should("be.visible")
+      .and("contain", "Must contain a valid routing number");
+    cy.getBySelLike("routingNumber-input").find("input").clear();
+
+    cy.getBySelLike("routingNumber-input").type("123456789").find("input").blur();
+    cy.get("#bankaccount-routingNumber-input-helper-text").should("not.exist");
+  }
+
+  verifyInvalidRoutingNumberAlert() {
+    cy.getBySelLike("routingNumber-input").type("12345678").find("input").blur();
+    cy.get("#bankaccount-routingNumber-input-helper-text")
+      .should("be.visible")
+      .and("contain", "Must contain a valid routing number");
+    cy.getBySelLike("routingNumber-input").find("input").clear();
+
+    cy.getBySelLike("routingNumber-input").type("123456789").find("input").blur();
+    cy.get("#bankaccount-routingNumber-input-helper-text").should("not.exist");
+  }
+
+  verifyEmptyRoutingNumberAlert() {
+    cy.getBySelLike("routingNumber-input").find("input").focus().blur();
+    cy.get(`#bankaccount-routingNumber-input-helper-text`)
+      .should("be.visible")
+      .and("contain", "Enter a valid bank routing number");
+  }
+
+  clickCreate() {
+    cy.getBySel("bankaccount-new").click();
+  }
+  
+  verifyEmptyBankNameAlert() {
+    cy.getBySelLike("bankName-input").type("The").find("input").clear().blur();
+    cy.get("#bankaccount-bankName-input-helper-text")
+      .should("be.visible")
+      .and("contain", "Enter a bank name");
+  }
+
+  verifyTooShortBankNameAlert() {
+    cy.getBySelLike("bankName-input").type("The").find("input").blur();
+    cy.get("#bankaccount-bankName-input-helper-text")
+      .should("be.visible")
+      .and("contain", "Must contain at least 5 characters");
+  }
+
+  verifySumbitButtonDisabled() {
+    cy.getBySel("bankaccount-submit").should("be.disabled");
+  }
+
+}
