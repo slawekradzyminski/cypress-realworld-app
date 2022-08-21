2216d7ca958657bfa84f336f41587684394e466a
LisowskiAdam <lisowskiadam99@gmail.com>
2022-08-21 11:48:03 +0200

    GitHub Dashboard2

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'cypress'
 
 export default defineConfig({
-  projectId: '7s5okt',
+  projectId: 'rjfvsj',
   viewportHeight: 1000,
   viewportWidth: 1280,
   retries: {
@@ -16,7 +16,6 @@ export default defineConfig({
       url: 'http://localhost:3001/__coverage__',
     },
   },
-  experimentalStudio: true,
   e2e: {
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
