4433c0ff93601d6987faeb9a3b738336ed234a9e
slawekradzyminski <slawomir.radzyminski@gmail.com>
2022-11-20 15:51:01 +0100

    Dashboard

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
-  projectId: "idamud",
+  projectId: "xpqzkn",
   viewportHeight: 1000,
   viewportWidth: 1280,
   retries: {
